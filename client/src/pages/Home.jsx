9b5f32c946c8b72eb5d0f676cd3df4c7320c6045
Katari <kataricoder@gmail.com>
2025-10-01 22:43:20 +0530

    adding api so that if user have cookies then it will directly redirect to their dashboard

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
+
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    const checkAuth = async () => {
+     const res = await axios.get("http://localhost:3000/api/hr/hrDetails", { withCredentials: true });
+      if (res.status == 200) {
+        navigate("/hr/overview");
+      }else{
+        navigate("/");
+      }
+    }
+    checkAuth();
+  }, []);
+
+
+
   return (
     <div>
        <div>
