0e790af0c664ffa178e0e53dae8bf0b798b7bedc
Katari <kataricoder@gmail.com>
2025-11-05 19:56:17 +0530

    making minnor change

--- server/routes/HrRoutes/HrRoute.js
+++ server/routes/HrRoutes/HrRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { loginHR, logoutHR, signupHR, verifyOtp } from "../../controllers/HrControllers/HrAuthControllers.js";
 import { hrMiddleware } from "../../Middlewere/HRMiddlewere/hrMiddlewere.js";
-import { addDepartment, getAllEmployees, getDepartments, getHRDetails } from "../../controllers/HrControllers/HrDashboardController.js";
+import { addDepartment, addProject, deleteProject, getAllEmployees, getDepartments, getHRDetails, getProjects } from "../../controllers/HrControllers/HrDashboardController.js";
 import { addEmployee } from "../../controllers/HrControllers/HREmployeeController.js";
 
 const router1 = express.Router();
@@ -15,6 +15,10 @@ router1.get("/getAllEmployees",hrMiddleware,getAllEmployees);
 
 router1.post("/addEmployee",hrMiddleware, addEmployee);
 router1.post("/addDepartment",hrMiddleware, addDepartment);
-router1.get("/getDepartments", hrMiddleware, getDepartments)
+router1.get("/getDepartments", hrMiddleware, getDepartments);
+router1.post("/addProject",hrMiddleware, addProject);
+router1.get("/getProjects",hrMiddleware, getProjects);
+router1.delete("/project/:id", hrMiddleware, deleteProject);
+
 
 export default router1;
