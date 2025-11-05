cc0d9345124d07b5f44085978f0796f6628e62c9
Katari <kataricoder@gmail.com>
2025-11-05 19:55:29 +0530

    Adding Delete Route

--- server/controllers/HrControllers/HrDashboardController.js
+++ server/controllers/HrControllers/HrDashboardController.js
@@ -7,18 +7,18 @@ import prisma from "../../utils/Prisma.js";
 
 
 export const getHRDetails = async (req, res) => {
-    try {
-        const hrId = req.hr.id; 
-        const hr = await prisma.hR.findUnique({ where: { id: hrId } });
-        if (!hr) {
-            return res.status(404).json({ success: false, message: "HR not found" });
-        }
-      
-        return res.json({ success: true, hr });
-        
-    } catch (error) {
-        console.error("Error fetching HR details:", error);
+  try {
+    const hrId = req.hr.id;
+    const hr = await prisma.hR.findUnique({ where: { id: hrId } });
+    if (!hr) {
+      return res.status(404).json({ success: false, message: "HR not found" });
     }
+
+    return res.json({ success: true, hr });
+
+  } catch (error) {
+    console.error("Error fetching HR details:", error);
+  }
 }
 //------------------------------------------------Get All Employees-------------------------------------------------------------------->
 
@@ -27,7 +27,7 @@ export const getAllEmployees = async (req, res) => {
   try {
     const employees = await prisma.employee.findMany({
       include: {
-        department: true, 
+        department: true,
       },
     });
 
@@ -50,7 +50,7 @@ export const addDepartment = async (req, res) => {
 
   try {
 
-    
+
     const existing = await prisma.department.findUnique({
       where: { name },
     });
@@ -66,7 +66,7 @@ export const addDepartment = async (req, res) => {
       data: { name },
     });
 
-    
+
     return res.status(201).json({
       success: true,
       message: "Department added successfully",
@@ -96,3 +96,80 @@ export const getDepartments = async (req, res) => {
     console.error("Error fetching departments:", error);
   }
 }
+
+//-----------------------------Add projects----------------------------------//
+
+export const addProject = async (req, res) => {
+  const { name, client, managerId, Deadline, employees } = req.body;
+
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        name,
+        client,
+        managerId,
+        Deadline: Deadline ? new Date(Deadline) : null,
+        members: {
+          connect: employees?.map((id) => ({ id })),
+        },
+      },
+      include: {
+        members: true,
+        manager: true,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Project added successfully",
+      project: newProject,
+    });
+  } catch (error) {
+    console.error("Error adding project:", error);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+
+//-----------------------------Get Projects----------------------------------//
+
+
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await prisma.project.findMany({
+      include: {
+        members: true,
+        manager: true,
+      }
+    });
+    res.json({ success: true, projects });
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+  }
+}
+
+//-----------------------------Delete Project----------------------------------//
+
+
+export const deleteProject = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleted = await prisma.project.delete({
+      where: { id }
+    });
+
+    return res.status(200).json({
+      message: "Project deleted successfully",
+      project: deleted,
+    });
+
+  } catch (error) {
+    console.error("Error deleting project:", error);
+
+    return res.status(500).json({
+      message: "Failed to delete project",
+      error: error.message,
+    });
+  }
+};
